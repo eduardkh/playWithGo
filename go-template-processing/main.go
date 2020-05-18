655eed12cbdd59efa53f59305e26c4c33104130e
Eduard Khiaev <eddietheone1984@gmail.com>
2020-05-18 17:08:19 +0300

    Brienne of house Tarth

---
+++ go-template-processing/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"demo/template"
+	"fmt"
+)
+
+func main() {
+	// data structure the template will be applied to
+	vars := make(map[string]interface{})
+	vars["Name"] = "Brienne"
+	vars["House"] = "Tarth"
+	vars["Traits"] = []string{"Brave", "Loyal"}
+
+	// process a template string
+	resultA := template.ProcessString("{{.Name}} of house {{.House}}", vars)
+
+	// process a template file
+	resultB := template.ProcessFile("templates/got.tmpl", vars)
+
+	fmt.Println(resultA, "\n")
+	fmt.Println(resultB)
+
+	// using a Struct
+	type Westerosi struct {
+		Name   string
+		House  string
+		Traits []string
+	}
+
+	jorah := Westerosi{"Ser Jorah", "Mormont", []string{"Brave", "Protective"}}
+	resultC := template.ProcessFile("templates/got.tmpl", jorah)
+
+	fmt.Println(resultC)
+}
